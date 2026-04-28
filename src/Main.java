a2ea75b85773f3ded32ce9d14f307b30a083eaa5
Luka Georgieski <89241332@student.upr.si>
2026-04-28 21:24:00 +0200

    Set up MPI (thx to some indian guy on yt)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import mpi.*;
 public class Main {
+    public static void main(String[] args) throws Exception {
+        MPI.Init(args);
+        int me = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        System.out.println("Hello, I am " + me + " and the size is " + size);
+        MPI.Finalize();
+    }
 }
